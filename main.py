96634e1b28ff5684f9c3a85cfa6abcfa6050c805
datawave05 <datawave05@gmail.com>
2023-12-31 11:21:36 +0530

    entity config

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pandas as pd
-
+from source.entity.config_entity import TrainingPipelineConfig
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    train_pipeline_config_obj = TrainingPipelineConfig()
+
+    print(train_pipeline_config_obj.__dict__)
 
 
