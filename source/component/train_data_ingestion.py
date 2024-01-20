173a5b1db77061632fad7cfb07447f2d06729fbd
datawave05 <datawave05@gmail.com>
2024-01-20 20:48:14 +0530

    data ingestion complete

--- source/component/train_data_ingestion.py
+++ source/component/train_data_ingestion.py
@@ -1,7 +1,9 @@
 import os
 import pandas as pd
+from pandas import DataFrame
 from source.exception import ChurnException
 from pymongo.mongo_client import MongoClient
+from sklearn.model_selection import train_test_split
 from source.logger import logging
 
 
@@ -9,7 +11,7 @@ class DataIngestion:
     def __init__(self, train_config):
         self.train_config = train_config
 
-    def export_data_into_feature_store(self):
+    def export_data_into_feature_store(self) -> DataFrame:
         try:
             logging.info("start: data load from mongoDB")
 
@@ -27,16 +29,28 @@ class DataIngestion:
 
             logging.info("complete: data load from mongoDB")
 
+            return data
+
         except ChurnException as e:
             logging.error(e)
             raise e
 
-    def split_data_test_train(self):
+    def split_data_test_train(self, data: DataFrame) -> None:
         try:
-            pass
+            logging.info("start: train, test data split")
+
+            train_set, test_set = train_test_split(data, test_size=self.train_config.train_test_split_ratio, random_state=42)
+
+            dir_path = os.path.dirname(self.train_config.train_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+
+            train_set.to_csv(self.train_config.train_file_path, index=False)
+            test_set.to_csv(self.train_config.test_file_path, index=False)
+
+            logging.info("complete: train, test data split")
         except ChurnException as e:
             raise e
 
     def initiate_data_ingestion(self):
-        self.export_data_into_feature_store()
-        self.split_data_test_train()
+        data = self.export_data_into_feature_store()
+        self.split_data_test_train(data)
