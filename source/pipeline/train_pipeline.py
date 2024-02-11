0d3bc244dfe1905f0d1c2b7d5bd16d41f56389ce
datawave05 <datawave05@gmail.com>
2024-02-11 10:48:35 +0530

    data transformation

--- source/pipeline/train_pipeline.py
+++ source/pipeline/train_pipeline.py
@@ -1,5 +1,6 @@
 from source.component.train_data_ingestion import DataIngestion
 from source.component.data_validation import DataValidation
+from source.component.data_transformation import DataTransformation
 from source.entity.config_entity import TrainingPipelineConfig
 
 
@@ -16,6 +17,11 @@ class TrainPipeline:
         data_validation_obj = DataValidation(self.utility_config)
         data_validation_obj.initiate_data_validation()
 
+    def start_data_transformation(self):
+        data_tras_obj = DataTransformation(self.utility_config)
+        data_tras_obj.initiate_data_transformation()
+
     def run_train_pipeline(self):
         self.start_data_ingestion()
         self.start_data_validation()
+        self.start_data_transformation()
